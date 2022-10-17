204f64aba9056fb160e9fa182090c02ce1b1492f
kholida magfirah <kholidamagfirah161001@gmail.com>
2022-10-17 17:57:11 +0700

    Kholida Magfirah

---
+++ UsingIntent/app/src/main/java/com/example/usingintent/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.usingintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    int request_Code = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        startActivityForResult(new Intent(".SecondActivity"), request_Code);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
